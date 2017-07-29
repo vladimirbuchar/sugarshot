632f97f4cf04a91e3d212039285aad903e527efa
Vladimir Buchar <vladimirbuchar@gmail.com>
2017-07-29 22:48:38 +0200

    Search

--- Kernel/Controllers/SettingsController.php
+++ Kernel/Controllers/SettingsController.php
@@ -141,10 +141,9 @@ abstract class SettingsController extends AdminController {
         $saveToSession = true;
         if (!empty($ajaxParametrs["SortColumn"]) && !empty($ajaxParametrs["SortType"]))
             $sort = new SortDatabase ($ajaxParametrs["SortType"], $ajaxParametrs["SortColumn"]);
-        $modelName =  "Model\\".$ajaxParametrs["ModelName"];
+            $modelName =  "Model\\".$ajaxParametrs["ModelName"];
         $sessionId = $ajaxParametrs["ModelName"];
         $model = new $modelName();
-        
         if (!empty($ajaxParametrs["SaveFiltrSortToSession"]))
         {
             $saveToSession = $ajaxParametrs["SaveFiltrSortToSession"] =="true" ? true:false;
--- Model/ContentVersion.php
+++ Model/ContentVersion.php
@@ -171,7 +171,7 @@ class ContentVersion extends DatabaseTable {
     public function Search($groupId, $langId, $searchString) {
         if (empty($searchString))
             return array();
-        return dibi::query("SELECT DISTINCT SearchValue, Name  FROM  SEARCHVIEW WHERE SSGroupId =%i AND LangId = %i AND  (SearchValue LIKE %~like~ OR Name LIKE %~like~) GROUP BY Id", $groupId, $langId, $searchString, $searchString)->fetchAll();
+        return dibi::query("SELECT DISTINCT SearchValue, Name,SeoUrl  FROM  SEARCHVIEW WHERE SSGroupId =%i AND LangId = %i AND  (SearchValue LIKE %~like~ OR Name LIKE %~like~) GROUP BY Id", $groupId, $langId, $searchString, $searchString)->fetchAll();
     }
 
     private function SaveData($data, $contentId, $langId = 0) {
